c4d1bde027044ba393d6a79b5ae11657404ed5cc
Sijin He <sijin@ebi.ac.uk>
2017-07-24 16:42:08 +0100

    update config

--- setup_backend_host.sh
+++ setup_backend_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ROOT_DIR=/etc/nginx/sites-available
 cp $ROOT_DIR/reverse-proxy.conf $ROOT_DIR/reverse-proxy.conf_backup
-sed "s@http://localhost/@http://$BACKEND_HOST/@g" $ROOT_DIR/reverse-proxy.conf_backup > $ROOT_DIR/reverse-proxy.conf
+sed "s@http://localhost:@http://$BACKEND_HOST:@g" $ROOT_DIR/reverse-proxy.conf_backup > $ROOT_DIR/reverse-proxy.conf
 ln -s $ROOT_DIR/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
 service nginx configtest
 service nginx restart
