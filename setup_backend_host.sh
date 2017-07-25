ae883e76c2731e53acc50b359e87c70d42fd13de
Sijin He <sijin@ebi.ac.uk>
2017-07-25 09:45:05 +0100

    remove nginx restart

--- setup_backend_host.sh
+++ setup_backend_host.sh
@@ -2,5 +2,3 @@
 ROOT_DIR=/etc/nginx/sites-enabled
 cp $ROOT_DIR/default /tmp/default_backup
 sed "s@http://localhost:@http://$BACKEND_HOST:@g" /tmp/default_backup > $ROOT_DIR/default
-service nginx configtest
-service nginx restart
