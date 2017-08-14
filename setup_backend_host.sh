0512080a9f17cc9be746f78bf2a988ab7a95ca90
Sijin He <sijin@ebi.ac.uk>
2017-08-14 11:10:43 +0100

    add app library url config

--- setup_backend_host.sh
+++ setup_backend_host.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ROOT_DIR=/etc/nginx/sites-enabled
 cp $ROOT_DIR/default /tmp/default_backup
-sed "s@http://localhost:@http://$BACKEND_HOST:@g" /tmp/default_backup > $ROOT_DIR/default
+sed "s@http://localhost:8888@http://$BACKEND_HOST:8888@g" /tmp/default_backup > $ROOT_DIR/default
+cp $ROOT_DIR/default /tmp/default_backup
+sed "s@http://localhost:80@http://$APPLIBRARY_HOST:80@g" /tmp/default_backup > $ROOT_DIR/default
